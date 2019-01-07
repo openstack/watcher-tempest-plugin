7f6442733a2060a52aadc59849f310bf8568fa89
chenke <chen.ke14@zte.com.cn>
2019-01-07 21:05:29 +0800

    add the scenario test for vm_workload_consolidation strategy
    Change-Id: Ia56933911ecac8947babbeb8d02fb1d3c83e02d6

---
+++ watcher_tempest_plugin/tests/scenario/test_execute_vm_workload_consolidation.py
@@ -0,0 +1,136 @@
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2019 ZTE Corporation
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import unicode_literals
+
+import functools
+
+from tempest import config
+from tempest.lib.common.utils import test_utils
+
+from watcher_tempest_plugin.tests.scenario import base
+
+CONF = config.CONF
+
+
+class TestExecuteVmWorkloadBalanceStrategy(base.BaseInfraOptimScenarioTest):
+    """Tests for action plans"""
+
+    GOAL_NAME = "server_consolidation"
+
+    @classmethod
+    def skip_checks(cls):
+        super(TestExecuteVmWorkloadBalanceStrategy, cls).skip_checks()
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestExecuteVmWorkloadBalanceStrategy, cls).resource_setup()
+        if CONF.compute.min_compute_nodes < 2:
+            raise cls.skipException(
+                "Less than 2 compute nodes, skipping multinode tests.")
+        if not CONF.compute_feature_enabled.live_migration:
+            raise cls.skipException("Live migration is not enabled")
+
+        cls.initial_compute_nodes_setup = cls.get_compute_nodes_setup()
+        enabled_compute_nodes = [cn for cn in cls.initial_compute_nodes_setup
+                                 if cn.get('status') == 'enabled']
+
+        cls.wait_for_compute_node_setup()
+
+        if len(enabled_compute_nodes) < 2:
+            raise cls.skipException(
+                "Less than 2 compute nodes are enabled, "
+                "skipping multinode tests.")
+
+    def test_execute_vm_workload_consolidation_action_plan(self):
+        """Execute an action plan based on the BASIC strategy
+
+        - create an audit template with the basic strategy
+        - run the audit to create an action plan
+        - get the action plan
+        - run the action plan
+        - get results and make sure it succeeded
+        """
+        self.addCleanup(self.rollback_compute_nodes_status)
+        instances = self._create_one_instance_per_host()
+        self._pack_all_created_instances_on_one_host(instances)
+        self._create_one_instance_per_host()
+
+        _, goal = self.client.show_goal(self.GOAL_NAME)
+        _, strategy = self.client.show_strategy("vm_workload_consolidation")
+        _, audit_template = self.create_audit_template(
+            goal['uuid'], strategy=strategy['uuid'])
+
+        self.assertTrue(test_utils.call_until_true(
+            func=functools.partial(
+                self.has_action_plans_finished),
+            duration=600,
+            sleep_for=2
+        ))
+
+        _, audit = self.create_audit(
+            audit_template['uuid'],
+            parameters={
+                "granularity": 1,
+                "period": 72000,
+                "aggregation_method": {"instance": "last", "node": "last"}
+            }
+        )
+
+        try:
+            self.assertTrue(test_utils.call_until_true(
+                func=functools.partial(
+                    self.has_audit_finished, audit['uuid']),
+                duration=600,
+                sleep_for=2
+            ))
+        except ValueError:
+            self.fail("The audit has failed!")
+
+        _, finished_audit = self.client.show_audit(audit['uuid'])
+        if finished_audit.get('state') in ('FAILED', 'CANCELLED', 'SUSPENDED'):
+            self.fail("The audit ended in unexpected state: %s!"
+                      % finished_audit.get('state'))
+
+        _, action_plans = self.client.list_action_plans(
+            audit_uuid=audit['uuid'])
+        action_plan = action_plans['action_plans'][0]
+
+        _, action_plan = self.client.show_action_plan(action_plan['uuid'])
+
+        if action_plan['state'] in ('SUPERSEDED', 'SUCCEEDED'):
+            # This means the action plan is superseded so we cannot trigger it,
+            # or it is empty.
+            return
+
+        # Execute the action by changing its state to PENDING
+        _, updated_ap = self.client.start_action_plan(action_plan['uuid'])
+
+        self.assertTrue(test_utils.call_until_true(
+            func=functools.partial(
+                self.has_action_plan_finished, action_plan['uuid']),
+            duration=600,
+            sleep_for=2
+        ))
+        _, finished_ap = self.client.show_action_plan(action_plan['uuid'])
+        _, action_list = self.client.list_actions(
+            action_plan_uuid=finished_ap["uuid"])
+        self.assertIn(updated_ap['state'], ('PENDING', 'ONGOING'))
+        self.assertIn(finished_ap['state'], ('SUCCEEDED', 'SUPERSEDED'))
+
+        for action in action_list['actions']:
+            self.assertEqual('SUCCEEDED', action.get('state'))
